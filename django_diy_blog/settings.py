d51c4fb7a2087c4bd5c586a5e802c81ef1c468c5
Godo Quaran <godoquaran@gmail.com>
2023-03-16 23:18:27 -0500

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- django_diy_blog/settings.py
+++ django_diy_blog/settings.py
@@ -27,8 +27,14 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'aipgg10+nb5xxb2rb^ekr8d8t#i3m1
 #DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-e8e1.up.railway.app', '127.0.0.1']
 
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://web-production-e8e1.up.railway.app']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 # Application definition
 
