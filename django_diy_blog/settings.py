b2aa61dfac1e9322a8c35c50823d1d263a2b28b3
Godo Quaran <godoquaran@gmail.com>
2023-03-16 21:18:45 -0500

    Finish All

--- django_diy_blog/settings.py
+++ django_diy_blog/settings.py
@@ -128,14 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Static file serving.
 # http://whitenoise.evans.io/en/stable/django.html#django-middleware
