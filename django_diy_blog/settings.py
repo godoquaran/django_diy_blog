83122cab4ab2e00000ce32fa16bc1c247a837a95
Godo Quaran <godoquaran@gmail.com>
2023-03-16 21:13:35 -0500

    Finish app with static files

--- django_diy_blog/settings.py
+++ django_diy_blog/settings.py
@@ -133,6 +133,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Static file serving.
 # http://whitenoise.evans.io/en/stable/django.html#django-middleware
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
