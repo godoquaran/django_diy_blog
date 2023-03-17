cfac484cbf08676f0c1ec12aa8aef27be4b365e1
Godo Quaran <godoquaran@gmail.com>
2023-03-16 21:00:01 -0500

    Finish App

--- django_diy_blog/settings.py
+++ django_diy_blog/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'RfUjXn2r5u7x!A%D*G-KaPdSgVkYp3
 #DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['web-production-90999.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-a721.up.railway.app', '127.0.0.1']
 
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-CSRF_TRUSTED_ORIGINS = ['https://web-production-90999.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-a721.up.railway.app']
 
 # During development/for this tutorial you can instead set just the base URL
 # CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
